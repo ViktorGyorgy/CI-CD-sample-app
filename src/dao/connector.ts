1d1254b50555a93e34fb02cc9906a3d9ca6b020e
VIKTOOOOOOOOOOOR <viktorgyorgy@gmail.com>
2024-01-30 22:21:05 +0100

    created hello world express app

---
+++ src/dao/connector.ts
@@ -0,0 +1,13 @@
+// import { MongoClient } from "mongodb";
+
+// const url = 'mongodb://localhost:27017';
+// const uri = "<connection string>";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri,  {
+//         serverApi: {
+//             version: ServerApiVersion.v1,
+//             strict: true,
+//             deprecationErrors: true,
+//         }
+//     }
+// );
